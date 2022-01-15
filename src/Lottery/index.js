9ddd6b1f13b2dbd86c73c05459f2c4e46690d126
Danny <dannyisadog10@gmail.com>
2022-01-15 17:38:11 +0800

    feat: inject store

--- src/Lottery/index.js
+++ src/Lottery/index.js
@@ -4,8 +4,10 @@ import TimeSetting from "./TimeSetting";
 import CountdownArea from "./CountdownArea";
 import CandidateBlock from "./CandidateBlock";
 import Result from "./Result";
+import StartButton from "./StartButton";
 
-import { createStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const Container = styled.div`
   padding: 15px;
@@ -15,27 +17,35 @@ const Container = styled.div`
     .countdown-area-wrapper {
       margin-left: 20px;
     }
+    .start-button-wrapper {
+      margin-left: 20px;
+    }
   }
 `;
 
 const Lottery = () => {
   return(
-    <Container>
-      <div className="wrapper">
-        <div className="time-setting-wrapper">
-          <TimeSetting />
+    <Provider store={store}>
+      <Container>
+        <div className="wrapper">
+          <div className="time-setting-wrapper">
+            <TimeSetting />
+          </div>
+          <div className="countdown-area-wrapper">
+            <CountdownArea />
+          </div>
+          <div className="start-button-wrapper">
+            <StartButton />
+          </div>
         </div>
-        <div className="countdown-area-wrapper">
-          <CountdownArea />
+        <div className="candidate-block-wrapper">
+          <CandidateBlock />
         </div>
-      </div>
-      <div className="candidate-block-wrapper">
-        <CandidateBlock />
-      </div>
-      <div className="result-wrapper">
-        <Result />
-      </div>
-    </Container>
+        <div className="result-wrapper">
+          <Result />
+        </div>
+      </Container>
+    </Provider>
   );
 }
 
